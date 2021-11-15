88b2c3a31caf027e284817b2346ca09750a0e017
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-15 18:18:04 -0300

    Add Auth Method to Routes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 import { ListUsersController, ListUsersByDptController } from './controllers/Users/ListUsersController'
 import { CreateUserController } from './controllers/Users/CreateUserController'
@@ -23,48 +24,48 @@ const router = Router()
 /* Endpoints to users */
 
 // List all users
-router.get('/users', new ListUsersController().handle)
+router.get('/users', ensureAuthenticated, new ListUsersController().handle)
 // List all users by Department
-router.get('/users/department/:id', new ListUsersByDptController().handle)
+router.get('/users/department/:id', ensureAuthenticated, new ListUsersByDptController().handle)
 // Create user
-router.post('/users', new CreateUserController().handle)
+router.post('/users', ensureAuthenticated, new CreateUserController().handle)
 // Update user
-router.put('/users/:id', new UpdateUserController().handle)
+router.put('/users/:id', ensureAuthenticated, new UpdateUserController().handle)
 // Delete user
-router.delete('/users/:id', new DeleteUserController().handle)
+router.delete('/users/:id', ensureAuthenticated, new DeleteUserController().handle)
 
 /* */
 
 /* Endpoints to offices */
 
 // Create cost
-router.post('/offices', new CreateOfficesController().handle)
+router.post('/offices', ensureAuthenticated, new CreateOfficesController().handle)
 
 /* */
 
 /* Endpoints to departments */
 
 // List all departments
-router.get('/departments', new ListDepartmentsController().handle)
+router.get('/departments', ensureAuthenticated, new ListDepartmentsController().handle)
 // List all departments by Costs Central
-router.get('/departments/costs-central/:id', new ListDepartmentsByDptController().handle)
+router.get('/departments/costs-central/:id', ensureAuthenticated, new ListDepartmentsByDptController().handle)
 // Create department
-router.post('/departments', new CreateDepartmentController().handle)
+router.post('/departments', ensureAuthenticated, new CreateDepartmentController().handle)
 // Update department
-router.put('/departments/:id', new UpdateDepartmentController().handle)
+router.put('/departments/:id', ensureAuthenticated, new UpdateDepartmentController().handle)
 // Delete department
-router.delete('/departments/:id', new DeleteDepartmentController().handle)
+router.delete('/departments/:id', ensureAuthenticated, new DeleteDepartmentController().handle)
 
 /* */
 
 /* Endpoints to costs-central */
 
 // Create cost
-router.post('/costs-central', new CreateCostsCentralController().handle)
+router.post('/costs-central', ensureAuthenticated, new CreateCostsCentralController().handle)
 // Update cost
-router.put('/costs-central/:id', new UpdateCostsCentralController().handle)
+router.put('/costs-central/:id', ensureAuthenticated, new UpdateCostsCentralController().handle)
 // Delete cost
-router.delete('/costs-central/:id', new DeleteCostsCentralController().handle)
+router.delete('/costs-central/:id', ensureAuthenticated, new DeleteCostsCentralController().handle)
 
 /* */
 
